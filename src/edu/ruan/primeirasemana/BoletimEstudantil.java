004eb4378f3d51fadd0838d9e4c6a6d7c0470999
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-04-26 21:30:41 -0300

    Anatomia das classes

---
+++ src/edu/ruan/primeirasemana/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+package edu.ruan.primeirasemana;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO!");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO!");
+    }
+}
