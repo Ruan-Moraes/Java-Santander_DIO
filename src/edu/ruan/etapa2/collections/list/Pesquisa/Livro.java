e232e7de4db3a404f2978627eb3ac331cebbfd5e
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-06-13 19:44:07 -0300

    Aula de Collections

---
+++ src/edu/ruan/etapa2/collections/list/Pesquisa/Livro.java
@@ -0,0 +1,35 @@
+package edu.ruan.etapa2.collections.list.Pesquisa;
+
+public class Livro {
+    //atributos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" + "titulo='" + titulo + '\'' + ", autor='" + autor + '\'' + ", anoPublicacao=" + anoPublicacao + '}';
+    }
+}
